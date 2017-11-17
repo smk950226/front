2a5aba334148c02db88510d7a20c5dd7c7048745
smk950226 <smk950226@naver.com>
2017-11-17 23:19:58 +0900

    Post에 대한 CRUD구현

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
@@ -6,6 +7,8 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.pk])
 
 class Comment(models.Model):
     post = models.ForeignKey(Post)
