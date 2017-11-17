082313f4198aa0c0f02f0be1cd677d93395e896a
smk950226 <smk950226@naver.com>
2017-11-17 23:48:08 +0900

    Comment에 대한 CRUUD구현

--- blog/models.py
+++ blog/models.py
@@ -10,8 +10,14 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse('blog:post_detail', args=[self.pk])
 
+    def __str__(self):
+        return self.title
+
 class Comment(models.Model):
     post = models.ForeignKey(Post)
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering=['-id']
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, resolve_url
 from django.views.generic import ListView, DetailView, UpdateView, DeleteView, CreateView
-from .models import Post
+from .models import Post, Comment
 from django.urls import reverse_lazy
 
 index = ListView.as_view(model=Post, template_name='blog/index.html')
@@ -18,3 +18,39 @@ class PostDeleteView(DeleteView):
 post_delete = PostDeleteView.as_view()
 
 post_new = CreateView.as_view(model=Post, fields='__all__')
+
+
+class CommentCreateView(CreateView):
+    model = Comment
+    fields = ['message']
+
+    def form_valid(self, form):
+        comment = form.save(commit=False)
+        comment.post = get_object_or_404(Post, pk=self.kwargs['post_pk'])
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return resolve_url(self.object.post)
+
+comment_new = CommentCreateView.as_view()
+
+
+class CommentUpdateView(UpdateView):
+    model = Comment
+    fields = ['message']
+
+    def get_success_url(self):
+        return resolve_url(self.object.post)
+
+
+comment_edit = CommentUpdateView.as_view()
+
+
+class CommentDeleteView(DeleteView):
+    model = Comment
+    
+    def get_success_url(self):
+        return resolve_url(self.object.post)
+
+
+comment_delete = CommentDeleteView.as_view()
