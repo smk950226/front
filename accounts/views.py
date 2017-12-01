4714986dae6dc995a779640a5ef95d922da67e86
smk950226 <smk950226@naver.com>
2017-12-01 23:42:16 +0900

    django upgrade전 백업

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
+from django.contrib.auth.views import LoginView as AuthLoginView
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
+
 
 class SignupFormView(CreateView):
     form_class = UserCreationForm
     template_name = 'accounts/signup_form.html'
     success_url = settings.LOGIN_URL
+
+class LoginView(AuthLoginView):
+    def get_template_names(self):
+        if self.request.is_ajax():
+            return ['accounts/_login.html']
+        return ['accounts/login.html']
+
 
 @login_required
 def profile(request):
