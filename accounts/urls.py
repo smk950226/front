04778518af686f8d796ad96135598b99b8a66d9e
smk950226 <smk950226@naver.com>
2017-12-01 23:03:01 +0900

    로그인/프로필 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
+from django.contrib.auth.views import login
 from . import views
 
 urlpatterns = [
     url(r'^signup/$', views.SignupFormView.as_view(), name='signup'),
+    url(r'^login/$', login, name='login', kwargs={'template_name': 'accounts/login.html'}),
+    url(r'^profile/$', views.profile, name='profile'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
 from django.conf import settings
+from django.contrib.auth.decorators import login_required
 
 class SignupFormView(CreateView):
     form_class = UserCreationForm
     template_name = 'accounts/signup_form.html'
     success_url = settings.LOGIN_URL
+
+@login_required
+def profile(request):
+    return render(request, 'accounts/profile.html')
